ea329172e55f4426400ab378d6919cc6c9ad8caa
payatsu <6873447+payatsu@users.noreply.github.com>
2019-12-18 22:27:10 +0900

    インストーラのURLをとりあえず1種類標準出力する動作を実装。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+extern crate regex;
+extern crate reqwest;
+
+use regex::Regex;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+	let tmp = reqwest::get("https://www.nvidia.co.jp/Download/processDriver.aspx?psid=98&pfid=764&rpf=1&osid=57&lang=en-us&ctk=0&dtid=1&dtcid=1")?.text()?;
+	let driver_result = reqwest::get(&tmp)?.text()?;
+
+	let mut re = Regex::new(
+		r#"(href=")(?P<url>/content/DriverDownload-March\d{4}/confirmation.php\?.+?)(")"#,
+	)?;
+	let confirmation = reqwest::get(
+		&[
+			"https://www.nvidia.co.jp",
+			&re.captures(&driver_result).unwrap()["url"],
+		]
+		.concat(),
+	)?
+	.text()?;
+
+	re = Regex::new(
+		r#"(href=")(?P<url>http://[[:alpha:]]{2}\.download\.nvidia\.com/Windows/.+?)(")"#,
+	)?;
+	println!("{}", &re.captures(&confirmation).unwrap()["url"]);
+
+	Ok(())
 }
