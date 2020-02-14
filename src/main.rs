c03ca80cd5913e6626789af6da3e197e1e9e1220
payatsu <6873447+payatsu@users.noreply.github.com>
2020-02-15 00:39:30 +0900

    refactor src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 extern crate regex;
 extern crate reqwest;
 
+use std::io::{Error, ErrorKind};
 use regex::Regex;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-	let tmp = reqwest::get("https://www.nvidia.co.jp/Download/processDriver.aspx?psid=98&pfid=764&rpf=1&osid=57&lang=en-us&ctk=0&dtid=1&dtcid=1")?.text()?;
-	let driver_result = reqwest::get(&tmp)?.text()?;
+	let mut response: reqwest::Response = reqwest::get("https://www.nvidia.co.jp/Download/processDriver.aspx?psid=98&pfid=764&rpf=1&osid=57&lang=en-us&ctk=0&dtid=1&dtcid=1")?;
+	if response.status() != reqwest::StatusCode::OK{
+		return Err(Box::new(Error::new(ErrorKind::Other, "")))
+	}
+	let driver_result = reqwest::get(&response.text()?)?.text()?;
 
 	let mut re = Regex::new(
 		r#"(href=")(?P<url>/content/DriverDownload-March\d{4}/confirmation.php\?.+?)(")"#,
